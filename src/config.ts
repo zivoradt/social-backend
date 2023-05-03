c9b9b75fc4ecc415a4bf81a36bed26acd055961f
zivoradt <zivoradtrickovic@gmail.com>
2023-05-03 12:04:10 +0200

    create email sender class

--- src/config.ts
+++ src/config.ts
@@ -15,6 +15,11 @@ class Config {
   public CLOUD_NAME: string | undefined;
   public CLOUD_API_KEY: string | undefined;
   public CLOUD_API_SECRET: string | undefined;
+  public SENDER_EMAIL: string | undefined;
+  public SENDER_EMAIL_PASSWORD: string | undefined;
+  public SENDGRID_API_KEY: string | undefined;
+  public SENDGRID_SENDER: string | undefined;
+
   private readonly DEFAULT_DATABSE_URL = 'mongodb://127.0.0.1:27017/chattyapp-backend';
 
   constructor() {
@@ -27,6 +32,10 @@ class Config {
     this.CLOUD_NAME = process.env.CLOUD_NAME || '';
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
+    this.SENDER_EMAIL = process.env.CLOUD_API_SECRET || '';
+    this.SENDER_EMAIL_PASSWORD = process.env.CLOUD_API_SECRET || '';
+    this.SENDGRID_API_KEY = process.env.CLOUD_API_SECRET || '';
+    this.SENDGRID_SENDER = process.env.CLOUD_API_SECRET || '';
   }
 
   public createLogger(name: string): buynan {
