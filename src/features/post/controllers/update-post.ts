d8bcf3f353c2cdf68a31c9ce9efd22b919529a5c
zivoradt <zivoradtrickovic@gmail.com>
2023-05-10 00:54:53 +0200

    update post router, controller and test added

--- src/features/post/controllers/update-post.ts
+++ src/features/post/controllers/update-post.ts
@@ -6,7 +6,10 @@ import { socketIOPostObject } from '@sockets/post';
 import { postQueue } from '@services/queues/post.queue';
 import { IPostDocument } from '@post/interfaces/post.interface';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
-import { postSchema } from '@post/schemas/post.schemes';
+import { postSchema, postWithImageSchema } from '@post/schemas/post.schemes';
+import { UploadApiResponse } from 'cloudinary';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { uploads } from '@global/helpers/cloudinary-upload';
 
 const postCache: PostCache = new PostCache();
 
@@ -31,5 +34,77 @@ export class Update{
 
     // Save updated post from redis to DB
     postQueue.addPostJob('updatePostInDB', {key: postId, value: postUpdated});
+
+    res.status(HTTP_STATUS.OK).json({message: 'Post updated successfully'});
   }
+
+
+  // Update post with same image
+  @joiValidation(postWithImageSchema)
+  public async postWithImage(req:Request,res:Response): Promise<void>{
+
+   const {imgId, imgVersion} = req.body;
+
+   if(imgId && imgVersion){
+    Update.prototype.updatePostWithImage(req);
+   }
+   else{
+      const result: UploadApiResponse = await Update.prototype.addImageToExistingPost(req);
+      if(!result.public_id){
+        throw new BadRequestError(result.message);
+      }
+   }
+    res.status(HTTP_STATUS.OK).json({message: 'Post with image updated successfully'});
+  }
+
+  private async updatePostWithImage(req: Request): Promise<void>{
+
+     // Extract value from request object
+     const { post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture } = req.body;
+     const {postId} = req.params;
+
+     // Create IPostDocument object to pass to redis
+     const updatedPost: IPostDocument = {
+       post, bgColor, feelings, privacy, gifUrl, imgVersion, imgId, profilePicture
+     } as IPostDocument;
+
+     // Save to redis and take a returning object of updated post to pass as object to save to DB
+     const postUpdated: IPostDocument = await postCache.updatePostInCache(postId, updatedPost);
+
+     // Sending updated post back to client
+     socketIOPostObject.emit('updated post', postUpdated, 'post');
+
+     // Save updated post from redis to DB
+     postQueue.addPostJob('updatePostInDB', {key: postId, value: postUpdated});
+  }
+
+  // Add new image to post
+  private async addImageToExistingPost(req: Request): Promise<UploadApiResponse>{
+
+    // Extract value from request object
+    const { post, bgColor, feelings, privacy, gifUrl, profilePicture, image } = req.body;
+    const {postId} = req.params;
+
+    // Upload image to cloudinary and return result (if is error - it will resolved where function is called)
+    const result: UploadApiResponse = await uploads(image) as UploadApiResponse;
+    if (!result?.public_id) {
+      return result;
+    }
+
+    // Create IPostDocument object to pass to redis
+    const updatedPost: IPostDocument = {
+      post, bgColor, feelings, privacy, gifUrl, imgId: result.public_id, imgVersion: result.version.toString(),  profilePicture
+    } as IPostDocument;
+
+    // Save to redis and take a returning object of updated post to pass as object to save to DB
+    const postUpdated: IPostDocument = await postCache.updatePostInCache(postId, updatedPost);
+
+    // Sending updated post back to client
+    socketIOPostObject.emit('updated post', postUpdated, 'post');
+
+    // Save updated post from redis to DB
+    postQueue.addPostJob('updatePostInDB', {key: postId, value: postUpdated});
+
+    return result;
+ }
 }
--- src/features/post/routes/postRoutes.ts
+++ src/features/post/routes/postRoutes.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from '@global/helpers/auth-middelware';
 import { Create } from '@post/controllers/create-post';
 import { Delete } from '@post/controllers/delete-post';
 import { Get } from '@post/controllers/get-post';
+import { Update } from '@post/controllers/update-post';
 import express, {Router} from 'express';
 
 
@@ -18,6 +19,8 @@ class PostRoutes {
     this.router.get('/post/images/:page', authMiddleware.checkAuthentification, Get.prototype.postsWithImages);
 
     this.router.delete('/post/:postId', authMiddleware.checkAuthentification, Delete.prototype.post);
+    this.router.put('/post/:postId', authMiddleware.checkAuthentification, Update.prototype.post);
+    this.router.put('/post/image/:postId', authMiddleware.checkAuthentification, Update.prototype.postWithImage);
     this.router.post('/post', authMiddleware.checkAuthentification, Create.prototype.post);
     this.router.post('/post/image/post', authMiddleware.checkAuthentification, Create.prototype.postWithImage);
 
